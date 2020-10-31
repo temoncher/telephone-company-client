aad2c5a0f674e8f3666277e755d531b83e1db22b
temoncher <temoncher@yandex.ru>
2020-10-31 18:59:20 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,284 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+  },
+  'extends': [
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/warnings',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    project: './tsconfig.json',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'react/prop-types': 0,
+    'semi': 'warn',
+    'comma-dangle': [
+      'warn',
+      'always-multiline',
+    ],
+    'no-console': 'error',
+    'quotes': [
+      'warn',
+      'single',
+      'avoid-escape',
+    ],
+    'jsx-quotes': [
+      'warn',
+      'prefer-double',
+    ],
+    'no-trailing-spaces': 'warn',
+    'indent': [
+      'warn',
+      2,
+    ],
+    'no-multiple-empty-lines': [
+      'warn',
+      {
+        'max': 1,
+      },
+    ],
+    'eol-last': [
+      'warn',
+      'always',
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      {
+        'blankLine': 'always',
+        'prev': [
+          'const',
+          'let',
+          'var',
+        ],
+        'next': '*',
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': [
+          'if',
+          'try',
+          'class',
+          'export',
+        ],
+      },
+      {
+        'blankLine': 'always',
+        'prev': [
+          'if',
+          'try',
+          'class',
+          'export',
+        ],
+        'next': '*',
+      },
+      {
+        'blankLine': 'any',
+        'prev': [
+          'const',
+          'let',
+          'var',
+          'export',
+        ],
+        'next': [
+          'const',
+          'let',
+          'var',
+          'export',
+        ],
+      },
+      {
+        'blankLine': 'always',
+        'prev': [
+          'expression',
+        ],
+        'next': [
+          'const',
+          'let',
+          'var',
+        ],
+      },
+    ],
+    'block-spacing': 'warn',
+    'eqeqeq': [
+      'warn',
+      'always',
+    ],
+    'comma-spacing': [
+      'warn',
+      {
+        'before': false,
+        'after': true,
+      },
+    ],
+    'func-call-spacing': [
+      'warn',
+      'never',
+    ],
+    'no-duplicate-imports': 'error',
+    'newline-before-return': 'warn',
+    'object-curly-spacing': [
+      'warn',
+      'always',
+      {
+        'arraysInObjects': true,
+        'objectsInObjects': true,
+      },
+    ],
+    'padded-blocks': [
+      'warn',
+      'never',
+    ],
+    'react/jsx-max-props-per-line': [
+      'warn',
+      {
+        'when': 'always',
+      },
+    ],
+    'react/jsx-first-prop-new-line': [
+      'warn',
+      'multiline',
+    ],
+    'react/jsx-closing-bracket-location': 'warn',
+    '@typescript-eslint/type-annotation-spacing': 'warn',
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        'multiline': {
+          'delimiter': 'semi',
+          'requireLast': true,
+        },
+      },
+    ],
+    'space-infix-ops': 'warn',
+    'object-property-newline': [
+      'warn',
+      {
+        'allowAllPropertiesOnSameLine': true,
+      },
+    ],
+    'arrow-spacing': 'warn',
+    'key-spacing': [
+      'warn',
+      {
+        'afterColon': true,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        'groups': [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'pathGroups': [
+          {
+            'pattern': '@/**',
+            'group': 'parent',
+            'position': 'before',
+          },
+          {
+            'pattern': 'react',
+            'group': 'external',
+            'position': 'before',
+          },
+        ],
+        'newlines-between': 'always',
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': false,
+        },
+        'pathGroupsExcludedImportTypes': [
+          'builtin',
+          'react',
+        ],
+      },
+    ],
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+  'overrides': [
+    {
+      'files': [
+        '*.test.tsx',
+      ],
+      'rules': {
+        'padding-line-between-statements': [
+          'warn',
+          {
+            'blankLine': 'always',
+            'prev': [
+              'const',
+              'let',
+              'var',
+            ],
+            'next': '*',
+          },
+          {
+            'blankLine': 'always',
+            'prev': '*',
+            'next': [
+              'if',
+              'try',
+              'class',
+              'export',
+            ],
+          },
+          {
+            'blankLine': 'always',
+            'prev': [
+              'if',
+              'try',
+              'class',
+              'export',
+            ],
+            'next': '*',
+          },
+          {
+            'blankLine': 'any',
+            'prev': [
+              'const',
+              'let',
+              'var',
+              'export',
+            ],
+            'next': [
+              'const',
+              'let',
+              'var',
+              'export',
+              'expression',
+            ],
+          },
+          {
+            'blankLine': 'always',
+            'prev': [
+              'expression',
+            ],
+            'next': [
+              'const',
+              'let',
+              'var',
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const ROOT = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  ROOT
+);
